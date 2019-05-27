84e8982862e31470db9e7a75a88d068b062faed3
Scott <me@niftybit.co.uk>
2019-05-27 15:23:56 +0100

    Fix for hello world

--- cpp-project/src/exercises/hello_world.cpp
+++ cpp-project/src/exercises/hello_world.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
+#include <CL/sycl.hpp>
+
 /* include the SYCL runtime header */
+class hello_world;
 
 int main (int argc, char *argv[]) {
+    /* construct a queue object called myQueue */
+    cl::sycl::default_selector selector;
 
-  /* construct a queue object called myQueue */
+    /* Queues are used to enqueue work.
+    * In this case we construct the queue using the selector. Users can create
+    * their own selectors to choose whatever kind of device they might need. */
+    cl::sycl::queue myQueue(selector);
 
-  myQueue.submit([&](cl::sycl::handler &cgh) {
+    myQueue.submit([&](cl::sycl::handler &cgh) {
+        /* construct a stream object */
+        cl::sycl::stream os(1024, 80, cgh);
 
-    /* construct a stream object */
-
-    cgh.single_task<class hello_world>([=]() {
-       
-       /* output "Hello World" to the console */
-        
+        cgh.single_task&lt;class hello_world&gt;([=]() {
+           /* output "Hello World" to the console */
+           os &lt;&lt; "Hello, World!\n";
+        });
     });
-  });
 }
