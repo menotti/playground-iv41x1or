0a2a275f68b2a1f4e62c84c9bfd1b3944657d420
Rod Burns <rod@codeplay.com>
2019-06-21 17:24:44 +0100

    Latest Content

--- cpp-project/src/exercises/sycl_debugging.cpp
+++ cpp-project/src/exercises/sycl_debugging.cpp
@@ -1,16 +1,17 @@
 #include <CL/sycl.hpp>
- namespace sycl = cl::sycl;
+namespace sycl = cl::sycl;
+class printkernel;
 
- int main(int, char**) {
-   sycl::queue queue(sycl::default_selector{});
+int main(int, char**) {
+  sycl::queue queue(sycl::default_selector{});
 
-   queue.submit([&] (sycl::handler& cgh) {
-       sycl::stream out(1024, 256, cgh);
+  queue.submit([&] (sycl::handler& cgh) {
+     sycl::stream out(1024, 256, cgh);
 
-       cgh.single_task<class printkernel>([=] {
+     cgh.single_task<class printkernel>([=] {
        out << "Hello stream!" << sycl::endl;
      } );
-     } );
+   } );
 
    return 0;
  }
